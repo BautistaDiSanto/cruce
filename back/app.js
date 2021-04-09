c493c34e2c9bf30bc5258ecae7fc33deddab35e9
Bautista <juanbautistadisanto@gmail.com>
2021-04-09 12:06:09 -0300

    products sort, pagination

--- back/app.js
+++ back/app.js
@@ -3,16 +3,23 @@ require("./modules/Product.js");
 const ProductService = require("./modules/ProductService");
 const express = require("express");
 const cors = require("cors");
+const { query } = require("express");
 const app = express();
-const port = 3000;
+const port = 3001;
 
 const productService = new ProductService();
-app.use(express.json());
 app.use(cors());
+app.options("*", cors());
+app.use(express.json());
 
 app.get("/products", async (req, res) => {
   try {
-    const products = await productService.getAllProducts();
+    const products = await productService.getAllProducts(
+      req.query.limits,
+      req.query.page,
+      req.query.orderColumn,
+      req.query.direction
+    );
     res.status(200).json(products);
   } catch (error) {
     res.status(400).json(error.message);
@@ -24,11 +31,11 @@ app.get("/products/:id", async (req, res) => {
     const product = await productService.getProductById(req.params.id);
     res.status(200).json(product);
   } catch (error) {
-    res.status(400).json(error.message);
+    res.status(404).json(error.message);
   }
 });
 
-app.post("/products", async (req, res) => {
+app.post("/product", async (req, res) => {
   try {
     const product = await productService.createProduct(req.body);
     res.status(200).json(product);
@@ -40,20 +47,21 @@ app.post("/products", async (req, res) => {
 app.delete("/products/:id", async (req, res) => {
   try {
     const product = await productService.deleteProduct(req.params.id);
-    res.status(204).json();
+    res.status(200).json();
   } catch (error) {
-    res.status(400).json(error.message);
+    res.status(404).json(error.message);
   }
 });
 
 app.put("/products/:id", async (req, res) => {
   try {
-    console.log("hola");
+    const product = await productService.editProduct(req.body, req.params.id);
+    res.status(200).json(product);
   } catch (error) {
     console.log(error.message);
   }
 });
 
 app.listen(port, () => {
-  console.log("servidor a su servicio en el puerto", port);
+  console.log("servidor en el puerto", port);
 });
--- back/modules/ProductService.js
+++ back/modules/ProductService.js
@@ -1,8 +1,9 @@
 const Product = require("./Product");
+const sequelize = require("../database/sequelize");
 
 const validateProduct = (body) => {
-  if (!body.image) throw new Error("nombre property missing");
-  if (!body.name) throw new Error("nombre property missing");
+  if (!body.image) throw new Error("image property missing");
+  if (!body.name) throw new Error("name property missing");
   if (!body.price) throw new Error("price property missing");
 };
 
@@ -23,16 +24,22 @@ class ProductService {
   async getProductById(id) {
     try {
       const productFounded = await Product.findByPk(id);
-      if (!productFounded) throw new Error("not found papurri");
+      if (!productFounded) throw new Error("product not found");
       return productFounded;
     } catch (error) {
       throw new Error(error.message);
     }
   }
-  async getAllProducts() {
+  async getAllProducts(limits, page, orderColumn, direction) {
+    const order = orderColumn && [[orderColumn, direction]];
     try {
-      const allProducts = await Product.findAll();
-      return allProducts;
+      const { count, rows } = await Product.findAndCountAll({
+        offset: (parseInt(page) - 1) * limits,
+        limit: parseInt(limits),
+        order,
+      });
+      const pages = Math.ceil(count / limits);
+      return { pages, rows };
     } catch (error) {
       throw new Error(error.message);
     }
@@ -41,17 +48,19 @@ class ProductService {
     try {
       const product = await Product.findByPk(id);
       product.destroy();
+      return true;
     } catch (error) {
       throw new Error(error.message);
     }
   }
-  async editProduct(body) {
+  async editProduct(body, id) {
     try {
       validateProduct(body);
       const product = await Product.findByPk(id);
+      product.image = body.image;
       product.name = body.name;
       product.price = body.price;
-      product.image = body.image;
+      await product.save();
     } catch (error) {
       throw new Error(error.message);
     }
